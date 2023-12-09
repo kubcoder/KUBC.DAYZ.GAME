5fddfd491ba928a7f027dc0f18344ec863b576f3
kot23rus <agorshenin@kubcoder.ru>
2023-12-09 12:24:05 +0300

    Изменил инструменты работы с файлом types.xml

--- KUBC.DAYZ.GAME/MissionFiles/DB/Types/Flags.cs
+++ KUBC.DAYZ.GAME/MissionFiles/DB/Types/Flags.cs
@@ -13,40 +13,178 @@ namespace KUBC.DAYZ.GAME.MissionFiles.DB.Types
     public class Flags
     {
         /// <summary>
+        /// Признак что набор флагов был изменен
+        /// </summary>
+        [XmlIgnore] public bool IsModify = false;
+        
+        /// <summary>
         /// Учитывать колличество итемов в контенерах хранения (бочки, ящики, палатки)
         /// </summary>
         [XmlAttribute("count_in_cargo")]
-        public int InCargo { get; set; }
+        public int InCargo 
+        {
+            get
+            {
+                if (CountInCargo)
+                    return 1;
+                return 0;
+            }
+            set
+            {
+                var oldValue = CountInCargo;
+                if (value > 0)
+                    CountInCargo = true;
+                else
+                    CountInCargo = false;
+                if (oldValue!=CountInCargo)
+                    IsModify = true;
+            }
+        }
+        /// <summary>
+        /// Учитывать колличество итемов в контенерах хранения (бочки, ящики, палатки)
+        /// </summary>
+        [XmlIgnore] public bool CountInCargo = false;
 
         /// <summary>
         /// Учитывать колличество в накопителях (шо не понятно)
         /// </summary>
         [XmlAttribute("count_in_hoarder")]
-        public int InHoarder { get; set; }
+        public int InHoarder
+        {
+            get
+            {
+                if (CountInHoarder)
+                    return 1;
+                return 0;
+            }
+            set
+            {
+                var oldValue = CountInHoarder;
+                if (value > 0)
+                    CountInHoarder = true;
+                else
+                    CountInHoarder = false;
+                if (oldValue != CountInHoarder)
+                    IsModify = true;
+            }
+        }
+        /// <summary>
+        /// Учитывать колличество в накопителях (шо не понятно)
+        /// </summary>
+        [XmlIgnore] public bool CountInHoarder = false;
 
         /// <summary>
         /// Считать сколько итемов лежит в мире, т.е. валяется где-то
         /// </summary>
         [XmlAttribute("count_in_map")]
-        public int InMap { get; set; }
+        public int InMap
+        {
+            get
+            {
+                if (CountInMap)
+                    return 1;
+                return 0;
+            }
+            set
+            {
+                var oldValue = CountInMap;
+                if (value > 0)
+                    CountInMap = true;
+                else
+                    CountInMap = false;
+                if (oldValue != CountInMap)
+                    IsModify = true;
+            }
+        }
+        /// <summary>
+        /// Считать сколько итемов лежит в мире, т.е. валяется где-то
+        /// </summary>
+        [XmlIgnore] public bool CountInMap = false;
+
 
         /// <summary>
         /// Считать итемы которые находятся у игроков
         /// </summary>
         [XmlAttribute("count_in_player")]
-        public int InPlayer { get; set; }
+        public int InPlayer
+        {
+            get
+            {
+                if (CountInPlayer)
+                    return 1;
+                return 0;
+            }
+            set
+            {
+                var oldValue = CountInPlayer;
+                if (value > 0)
+                    CountInPlayer = true;
+                else
+                    CountInPlayer = false;
+                if (oldValue != CountInPlayer)
+                    IsModify = true;
+            }
+        }
+        /// <summary>
+        /// Считать итемы которые находятся у игроков
+        /// </summary>
+        [XmlIgnore]
+        public bool CountInPlayer = false;
 
         /// <summary>
         /// Считать итемы которые были созданы игроками
         /// </summary>
         [XmlAttribute("crafted")]
-        public int Crafted { get; set; }
+        public int Crafted
+        {
+            get
+            {
+                if (CountCrafted)
+                    return 1;
+                return 0;
+            }
+            set
+            {
+                var oldValue = CountCrafted;
+                if (value > 0)
+                    CountCrafted = true;
+                else
+                    CountCrafted = false;
+                if (oldValue != CountCrafted)
+                    IsModify = true;
+            }
+        }
+        /// <summary>
+        /// Считать итемы которые были созданы игроками
+        /// </summary>
+        [XmlIgnore] public bool CountCrafted = false;   
 
         /// <summary>
         /// Разрешить удаление итемов, для переспавна
         /// </summary>
         [XmlAttribute("deloot")]
-        public int Deloot { get; set; }
-
+        public int Deloot
+        {
+            get
+            {
+                if (DelootEnable)
+                    return 1;
+                return 0;
+            }
+            set
+            {
+                var oldValue = DelootEnable;
+                if (value > 0)
+                    DelootEnable = true;
+                else
+                    DelootEnable = false;
+                if (oldValue != DelootEnable)
+                    IsModify = true;
+            }
+        }
+        /// <summary>
+        /// Разрешить удаление итемов, для переспавна
+        /// </summary>
+        [XmlIgnore] public bool DelootEnable = false;
     }
 }
--- KUBC.DAYZ.GAME/MissionFiles/DB/Types/Item.cs
+++ KUBC.DAYZ.GAME/MissionFiles/DB/Types/Item.cs
@@ -10,53 +10,213 @@ namespace KUBC.DAYZ.GAME.MissionFiles.DB.Types
     public class Item
     {
         /// <summary>
+        /// Элемент был изменен
+        /// </summary>
+        [XmlIgnore]
+        public bool IsModified = false;
+
+        /// <summary>
+        /// Узнать были ли изменения в элементе.
+        /// </summary>
+        /// <remarks>
+        /// Не отображает изменения если поле <see cref="Flags"/> было обнулено, или создано без изменений.
+        /// </remarks>
+        /// <returns>
+        /// Истина если были изменения с момента создания или сброса флага <see cref="IsModified"/>
+        /// </returns>
+        public bool HasChanges()
+        {
+            if (IsModified) return true;
+            if (Flags!=null)
+            {
+                return Flags.IsModify;
+            }
+            return false;
+        }
+        
+        /// <summary>
         /// Название класса итема
         /// </summary>
         [XmlAttribute(AttributeName = "name")]
-        public string? Name { get; set; }
+        public string? Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                if (value!=_Name)
+                {
+                    _Name = value;
+                    IsModified = true;
+                }
+            }
+        }
+        private string? _Name;
+        
 
         /// <summary>
         /// Номинальное кол-во итема на карте
         /// </summary>
         [XmlElement("nominal")]
-        public int? Nominal { get; set; }
+        public int? Nominal
+        {
+            get { return _Nominal;}
+            set 
+            {
+                if (value != _Nominal)
+                {
+                    _Nominal = value;
+                    IsModified = true;
+                }
+            }
+        }
+        private int? _Nominal;
+        /// <summary>
+        /// Проверка на разрешение сериализации <see cref="Nominal"/>
+        /// </summary>
+        /// <returns>Если поле null то сериализация не выполняется</returns>
+        public bool ShouldSerializeNominal() { return Nominal.HasValue; }
+
 
         /// <summary>
         /// Время жизни итема в секундах. 
         /// Т.е. сколько он живет на сервере от момента появления. По истечении этого срока обычно удаляется.
         /// </summary>
         [XmlElement("lifetime")]
-        public int? LifeTime { get; set; }
+        public int? LifeTime
+        {
+            get { return _LifeTime; }
+            set
+            {
+                if (value != _LifeTime)
+                {
+                    _LifeTime = value;
+                    IsModified = true;
+                }
+            }
+        }
+        private int? _LifeTime;
+        /// <summary>
+        /// Проверка на разрешение сериализации <see cref="LifeTime"/>
+        /// </summary>
+        /// <returns>Если поле null то сериализация не выполняется</returns>
+        public bool ShouldSerializeLifeTime() { return LifeTime.HasValue; }
+
 
         /// <summary>
         /// Через сколько времени выполнить переразмещение элементов. Используется центральной экономикой.
         /// </summary>
         [XmlElement("restock")]
-        public int? Restock { get; set; }
+        public int? Restock
+        {
+            get { return _Restock; }
+            set
+            {
+                if (value != _Restock)
+                {
+                    _Restock = value;
+                    IsModified = true;
+                }
+            }
+        }
+        private int? _Restock;
+        /// <summary>
+        /// Проверка на разрешение сериализации <see cref="Restock"/>
+        /// </summary>
+        /// <returns>Если поле null то сериализация не выполняется</returns>
+        public bool ShouldSerializeRestock() { return Restock.HasValue; }
 
         /// <summary>
         /// Минимальное число итема в игре после снижения до этого кол-ва центральная экономика начинает спавн данного итема 
         /// </summary>
         [XmlElement("min")]
-        public int? Min { get; set; }
+        public int? Min
+        {
+            get { return _Min; }
+            set
+            {
+                if (value != _Min)
+                {
+                    _Min = value;
+                    IsModified = true;
+                }
+            }
+        }
+        private int? _Min;
+        /// <summary>
+        /// Проверка на разрешение сериализации <see cref="Min"/>
+        /// </summary>
+        /// <returns>Если поле null то сериализация не выполняется</returns>
+        public bool ShouldSerializeMin() { return Min.HasValue; }
 
         /// <summary>
         /// Минимальное наполнение итема при спавне (т.е. наполнение итема)
         /// </summary>
         [XmlElement("quantmin")]
-        public int? QuantMin { get; set; }
-
+        public int? QuantMin
+        {
+            get { return _QuantMin; }
+            set
+            {
+                if (value != _QuantMin)
+                {
+                    _QuantMin = value;
+                    IsModified = true;
+                }
+            }
+        }
+        private int? _QuantMin;
+        /// <summary>
+        /// Проверка на разрешение сериализации <see cref="QuantMin"/>
+        /// </summary>
+        /// <returns>Если поле null то сериализация не выполняется</returns>
+        public bool ShouldSerializeQuantMin() { return QuantMin.HasValue; }
         /// <summary>
         /// Максимальное наполнение итема при спавне (т.е. наполнение итема)
         /// </summary>
         [XmlElement("quantmax")]
-        public int? QuantMax { get; set; }
-
+        public int? QuantMax
+        {
+            get { return _QuantMax; }
+            set
+            {
+                if (value != _QuantMax)
+                {
+                    _QuantMax = value;
+                    IsModified = true;
+                }
+            }
+        }
+        private int? _QuantMax;
+        /// <summary>
+        /// Проверка на разрешение сериализации <see cref="QuantMax"/>
+        /// </summary>
+        /// <returns>Если поле null то сериализация не выполняется</returns>
+        public bool ShouldSerializeQuantMax() { return QuantMax.HasValue; }
         /// <summary>
         /// Цена итема при спавне лута. Чем выше число тем чаще экономика будет заниматься данным итемом
         /// </summary>
         [XmlElement("cost")]
-        public int? Cost { get; set; }
+        public int? Cost
+        {
+            get { return _Cost; }
+            set
+            {
+                if (value != _Cost)
+                {
+                    _Cost = value;
+                    IsModified = true;
+                }
+            }
+        }
+        private int? _Cost;
+        /// <summary>
+        /// Проверка на разрешение сериализации <see cref="Cost"/>
+        /// </summary>
+        /// <returns>Если поле null то сериализация не выполняется</returns>
+        public bool ShouldSerializeCost() { return Cost.HasValue; }
 
         /// <summary>
         /// Флаги спавна.
@@ -72,6 +232,44 @@ namespace KUBC.DAYZ.GAME.MissionFiles.DB.Types
         /// </summary>
         [XmlElement(ElementName = "category", IsNullable = false)]
         public List<Category>? Category { get; set; }
+        /// <summary>
+        /// Обновляем список категорий
+        /// </summary>
+        /// <param name="nCategories">Новый набор категорий</param>
+        public void UpdateCategory(IEnumerable<string> nCategories)
+        {
+            if (Category != null)
+            {
+                int i = 0;
+                while (i < Category.Count)
+                {
+                    if (nCategories.Contains(Category[i].Name))
+                    {
+                        i++;
+                    }
+                    else
+                    {
+                        IsModified = true;
+                        Category.RemoveAt(i);
+                    }
+                }
+            }
+            if (nCategories.Any())
+            {
+                Category ??= [];
+                foreach (var nCat in nCategories)
+                {
+                    if (!Category.Where(x => x.Name == nCat).Any())
+                    {
+                        Category.Add(new Category() { Name = nCat });
+                        IsModified = true;
+                    }
+                }
+            }
+            if ((Category != null) && (Category.Count==0))
+                Category = null;
+        }
+
 
         /// <summary>
         /// В каких зонах Tier разрешен спавн данного итема
@@ -81,7 +279,43 @@ namespace KUBC.DAYZ.GAME.MissionFiles.DB.Types
         /// </remarks>
         [XmlElement(ElementName = "value", IsNullable = false)]
         public List<Value>? Values { get; set; }
-
+        /// <summary>
+        /// Обновляем список зон спавна
+        /// </summary>
+        /// <param name="nValues">Новый набор зон</param>
+        public void UpdateValues(IEnumerable<string> nValues)
+        {
+            if (Values != null)
+            {
+                int i = 0;
+                while (i < Values.Count)
+                {
+                    if (nValues.Contains(Values[i].Name))
+                    {
+                        i++;
+                    }
+                    else
+                    {
+                        IsModified = true;
+                        Values.RemoveAt(i);
+                    }
+                }
+            }
+            if (nValues.Any())
+            {
+                Values ??= [];
+                foreach (var nValue in nValues)
+                {
+                    if (!Values.Where(x => x.Name == nValue).Any())
+                    {
+                        Values.Add(new Value() { Name = nValue });
+                        IsModified = true;
+                    }
+                }
+            }
+            if ((Values != null) && (Values.Count==0))
+                Values = null;
+        }
 
         /// <summary>
         /// В каких объектах может спавнится данный лут, например военные объекты, полиция или что то еще.
@@ -91,6 +325,43 @@ namespace KUBC.DAYZ.GAME.MissionFiles.DB.Types
         /// </remarks>
         [XmlElement(ElementName = "usage", IsNullable = false)]
         public List<Usage>? Usages { get; set; }
+        /// <summary>
+        /// Обновляем список использований
+        /// </summary>
+        /// <param name="nUsages">Новый набор использований</param>
+        public void UpdateUsages(IEnumerable<string> nUsages)
+        {
+            if (Usages != null)
+            {
+                int i = 0;
+                while (i < Usages.Count)
+                {
+                    if (nUsages.Contains(Usages[i].Name))
+                    {
+                        i++;
+                    }
+                    else
+                    {
+                        IsModified = true;
+                        Usages.RemoveAt(i);
+                    }
+                }
+            }
+            if (nUsages.Any())
+            {
+                Usages ??= [];
+                foreach (var nUsage in nUsages)
+                {
+                    if (!Usages.Where(x => x.Name == nUsage).Any())
+                    {
+                        Usages.Add(new Usage() { Name = nUsage });
+                        IsModified = true;
+                    }
+                }
+            }
+            if ((Usages != null) && (Usages.Count == 0))
+                Usages = null;
+        }
 
         /// <summary>
         /// Таг спавна. Т.е. где размещать, на полу или прямо на земле.
@@ -99,6 +370,43 @@ namespace KUBC.DAYZ.GAME.MissionFiles.DB.Types
         /// Возможные значения определены в файле cfglimitsdefinition.xml
         /// </remarks>
         [XmlElement(ElementName = "tag", IsNullable = false)]
-        public List<Tag>? Tags { get; set; } 
+        public List<Tag>? Tags { get; set; }
+        /// <summary>
+        /// Обновляем список тэгов
+        /// </summary>
+        /// <param name="ntags">Новый набор тэгов</param>
+        public void UpdateTags(IEnumerable<string> ntags)
+        {
+            if (Tags != null) 
+            {
+                int i = 0;
+                while (i < Tags.Count)
+                {
+                    if (ntags.Contains(Tags[i].Name))
+                    {
+                        i++;
+                    }
+                    else
+                    {
+                        IsModified = true;
+                        Tags.RemoveAt(i);
+                    }
+                }
+            }
+            if (ntags.Any())
+            {
+                Tags ??= [];
+                foreach(var ntag in ntags)
+                {
+                    if (!Tags.Where(x=> x.Name == ntag).Any())
+                    {
+                        Tags.Add(new Tag() { Name = ntag });
+                        IsModified = true;
+                    }
+                }
+            }
+            if ((Tags!=null)&&(Tags.Count == 0))
+                Tags = null;
+        }
     }
 }
