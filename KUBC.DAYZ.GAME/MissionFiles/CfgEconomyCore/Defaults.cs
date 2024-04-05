f39151618d546ba907b3c02263cc8f49b63ee6a2
kot23rus <agorshenin@kubcoder.ru>
2024-04-05 15:58:07 +0300

    Потерли лишнее, что бы глаза не мозолило... и начинаем возвращать по маленьку

--- KUBC.DAYZ.GAME/MissionFiles/CfgEconomyCore/Defaults.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace KUBC.DAYZ.GAME.MissionFiles.CfgEconomyCore
-{
-    /// <summary>
-    /// Набор параметров работы центральной экономики
-    /// </summary>
-    public class Defaults : List<DefaultEntity>
-    {
-    }
-}
